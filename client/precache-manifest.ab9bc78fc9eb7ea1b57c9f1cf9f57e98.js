2a766ff3089ad9c39fa71942a729ac9ab93c419c
Sonal <engineersonal@rediffmail.com>
2019-06-30 17:17:10 +0530

    first commit

---
+++ client/precache-manifest.ab9bc78fc9eb7ea1b57c9f1cf9f57e98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d1bf223d03a50b55bd19b240a7a502f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d38ec04df0fd8fd49a97",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "dc859fa693e8130d3c07",
+    "url": "/static/js/2.bf880dac.chunk.js"
+  },
+  {
+    "revision": "d38ec04df0fd8fd49a97",
+    "url": "/static/js/main.96c96047.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
